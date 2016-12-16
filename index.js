d800f965b45aca8ad59f6093f57de54cd3c318d1
Mike Taylor <mike@indexdata.com>
2016-12-16 15:05:41 +0000

    Many trivial whitespace changes to satisfy ESLint (STRIPES-100)

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ import TenantEdit from './tenants/TenantEdit';
 import ModuleList from './modules/ModuleList';
 import ModuleAdd from './modules/ModuleAdd';
 import ModuleEdit from './modules/ModuleEdit';
-import Health from './health/Health.js';
-import HealthConnected from './health/HealthConnected.js';
+import Health from './health/Health';
+import HealthConnected from './health/HealthConnected';
 
 
-export default ({pathname}) => <div>
-  <ConsoleMenu/>
-  <hr/>
-  <Match exactly pattern={`${pathname}/tenants`} component={TenantList}/>
-  <Match pattern={`${pathname}/tenants/edit/:tenantid`} component={TenantEdit}/> 
-  <Match pattern={`${pathname}/tenants/add`} component={TenantAdd}/>
-  <Match exactly pattern={`${pathname}/modules`} component={ModuleList}/>
-  <Match pattern={`${pathname}/modules/edit/:moduleid`} component={ModuleEdit}/> 
-  <Match pattern={`${pathname}/modules/add`} component={ModuleAdd}/>
-  <Match exactly pattern={`${pathname}/health/health`} component={Health}/>
-  <Match exactly pattern={`${pathname}/health/healthconnected`} component={HealthConnected}/>
+export default ({ pathname }) => <div>
+  <ConsoleMenu />
+  <hr />
+  <Match exactly pattern={`${pathname}/tenants`} component={TenantList} />
+  <Match pattern={`${pathname}/tenants/edit/:tenantid`} component={TenantEdit} />
+  <Match pattern={`${pathname}/tenants/add`} component={TenantAdd} />
+  <Match exactly pattern={`${pathname}/modules`} component={ModuleList} />
+  <Match pattern={`${pathname}/modules/edit/:moduleid`} component={ModuleEdit} />
+  <Match pattern={`${pathname}/modules/add`} component={ModuleAdd} />
+  <Match exactly pattern={`${pathname}/health/health`} component={Health} />
+  <Match exactly pattern={`${pathname}/health/healthconnected`} component={HealthConnected} />
 </div>;
