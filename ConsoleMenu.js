2da02bde498fcf2dcae4d54308cc0aa9c9e77e45
Mike Taylor <mike@indexdata.com>
2016-12-16 14:54:07 +0000

    Use a pure-function component rather than a class: fixes an ESLint prefer-stateless-function error (STRIPES-100)

--- ConsoleMenu.js
+++ ConsoleMenu.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-export default class ConsoleMenu extends Component {
-
-  render() {
-    return <div>Okapi Console Menu
-      [<Link to={'/okapi-console/tenants'}>Tenants</Link>]
-      [<Link to={'/okapi-console/modules'}>Modules</Link>]
-      [<Link to={'/okapi-console/health/health'}>Health</Link>]
-      [<Link to={'/okapi-console/health/healthconnected'}>Health Connected</Link>]
-    </div>;
-  }
+export default function(props) {
+  return <div>
+    Okapi Console Menu
+    [<Link to={'/okapi-console/tenants'}>Tenants</Link>]
+    [<Link to={'/okapi-console/modules'}>Modules</Link>]
+    [<Link to={'/okapi-console/health/health'}>Health</Link>]
+    [<Link to={'/okapi-console/health/healthconnected'}>Health Connected</Link>]
+  </div>;
 }
