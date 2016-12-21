c0f0a596e6b93b0a469eb0cbe75b09a265175ce7
Mike Taylor <mike@indexdata.com>
2016-12-21 13:45:13 +0000

    Do not pass tenantid directly as a prop to <ModuleSelector>, as it's not used. (The tenantid in props.params is used -- see STRIPES-125.)

--- tenants/TenantForm.js
+++ tenants/TenantForm.js
@@ -70,7 +70,7 @@ class TenantForm extends Component {
           </ButtonGroup>
         </Form>
         <br/>
-        {tenantid ? <ModuleSelector tenantid={tenantid} params={this.props.params} /> : null}
+        {tenantid ? <ModuleSelector params={this.props.params} /> : null}
       </div>
 
     );
