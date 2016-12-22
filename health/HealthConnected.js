473420945fcdd6923ea87fe20f9becdfee614829
Mike Taylor <mike@indexdata.com>
2016-12-22 17:43:05 +0000

    Vertical ws.

--- health/HealthConnected.js
+++ health/HealthConnected.js
@@ -4,15 +4,20 @@ import { connect } from 'stripes-connect'; // eslint-disable-line
 const Row = (props) => {
   const { instId, srvcId, healthMessage, healthStatus } = props.h;
 
-  let href = "#/okapi-console/modules/edit/" + srvcId;
+  const href = '#/okapi-console/modules/edit/' + srvcId;
   return <tr>
     <td>{instId}</td>
     <td><a href={href}>{srvcId}</a></td>
     <td>{props.map[srvcId]}</td>
     <td>{healthMessage}</td>
-    <td>{healthStatus ? "true" : "false"}</td>
-   </tr>
-}
+    <td>{healthStatus ? 'true' : 'false'}</td>
+  </tr>;
+};
+Row.propTypes = {
+  h: React.PropTypes.object.isRequired,
+  map: React.PropTypes.object.isRequired,
+};
+
 
 class Health extends Component {
   static manifest = { 'health':   { type: 'okapi',
@@ -25,34 +30,34 @@ class Health extends Component {
     const { health, modules } = this.props.data;
 
     if (!health) {
-      return <div/>
+      return <div />;
     }
 
-    console.log("Health.render: ",
-                "health = " + typeof(health) + ": ", health, "; ",
-                "modules = " + typeof(modules) + ": ", modules);
-    let moduleId2name = {};
+    console.log('Health.render: ',
+                'health = ' + typeof health + ': ', health, '; ',
+                'modules = ' + typeof modules + ': ', modules);
+    const moduleId2name = {};
     if (modules) {
       for (let i = 0; i < modules.length; i++) {
-        let module = modules[i];
+        const module = modules[i];
         moduleId2name[module.id] = module.name;
       }
     }
 
     return <table>
-             <thead>
-              <tr>
-               <th>Instance ID</th>
-               <th>Service ID</th>
-               <th>Module Name</th>
-               <th>Message</th>
-               <th>Status</th>
-              </tr>
-             </thead>
-             <tbody>
-              {health.map((h, index) => { return <Row key={index} h={h} map={moduleId2name}/> })}
-             </tbody>
-            </table>
+      <thead>
+        <tr>
+          <th>Instance ID</th>
+          <th>Service ID</th>
+          <th>Module Name</th>
+          <th>Message</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {health.map((h, index) => <Row key={index} h={h} map={moduleId2name} />)}
+      </tbody>
+    </table>;
   }
 }
 
