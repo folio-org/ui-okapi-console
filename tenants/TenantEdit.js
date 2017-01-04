9a9c53692fdddd670813af7a56aee46613590a31
Mike Taylor <mike@indexdata.com>
2017-01-04 15:44:14 +0000

    Fix all ESLint errors in TenantEdit.js

--- tenants/TenantEdit.js
+++ tenants/TenantEdit.js
@@ -1,41 +1,58 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'stripes-connect';
+import { connect } from 'stripes-connect'; // eslint-disable-line
 import TenantForm from './TenantForm';
 
 
 class TenantEdit extends Component {
+  static propTypes = {
+    mutator: React.PropTypes.shape({
+      tenants: React.PropTypes.shape({
+        PUT: React.PropTypes.func.isRequired,
+      }),
+    }),
+    data: React.PropTypes.shape({}),
+    params: React.PropTypes.shape({}),
+  };
+
   static contextTypes = {
     router: PropTypes.object.isRequired
   };
 
-  static manifest = { 'tenants': { type: 'okapi',
-                                   path: '_/proxy/tenants/:{tenantid}'
-                                 }
-                    };
+  static manifest = {
+    tenants: {
+      type: 'okapi',
+      path: '_/proxy/tenants/:{tenantid}'
+    }
+  };
 
-  cancel (data) {
-    this.context.router.transitionTo('/okapi-console/tenants'); 
+  constructor(props) {
+    super(props);
+    this.update = this.update.bind(this);
+    this.cancel = this.cancel.bind(this);
+  }
+
+  cancel() {
+    this.context.router.transitionTo('/okapi-console/tenants');
   }
 
   update(data) {
-    this.props.mutator.tenants.PUT(data).then (() =>
+    this.props.mutator.tenants.PUT(data).then(() =>
       this.context.router.transitionTo('/okapi-console/tenants')
       );
   }
 
   render() {
-      const { data: {tenants}, params: {tenantid} } = this.props;
+    const { data: { tenants }, params: { tenantid } } = this.props;
+    const tenant = tenants.find(t => t.id === tenantid);
 
-      let tenant = tenants.find((tenant) =>  { return tenant.id === tenantid });
-
-      return <TenantForm onSubmit={this.update.bind(this)}
-                         cancelForm={this.cancel.bind(this)}
-                         submitLabel='Save'
-                         initialValues={tenant}
-                         params={this.props.params} />
+    return <TenantForm
+      onSubmit={this.update}
+      cancelForm={this.cancel}
+      submitLabel="Save"
+      initialValues={tenant}
+      params={this.props.params}
+    />;
   }
 }
 
 export default connect(TenantEdit, 'okapi-console');
-
-
