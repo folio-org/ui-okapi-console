31b0f43ae9af69bf0c6527d9a9ab46a0c4e0c9f1
Mike Taylor <mike@indexdata.com>
2017-01-04 12:43:45 +0000

    Fix all ESLint errors in TenantAdd.js

--- tenants/TenantAdd.js
+++ tenants/TenantAdd.js
@@ -1,34 +1,50 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'stripes-connect';
+import { connect } from 'stripes-connect'; // eslint-disable-line
 import TenantForm from './TenantForm';
 
 class TenantAdd extends Component {
+  static propTypes = {
+    mutator: React.PropTypes.shape({
+      tenants: React.PropTypes.shape({
+        POST: React.PropTypes.func.isRequired,
+      })
+    }),
+  };
+
   static contextTypes = {
     router: PropTypes.object.isRequired
   };
 
-  static manifest = { 'tenants': { type: 'okapi',
-                                   path: '_/proxy/tenants',
-                                   fetch: false
-                                 }
-                    };
+  static manifest = {
+    tenants: {
+      type: 'okapi',
+      path: '_/proxy/tenants',
+      fetch: false
+    }
+  };
+
+  constructor(props) {
+    super(props);
+    this.create = this.create.bind(this);
+    this.cancel = this.cancel.bind(this);
+  }
 
   create(data) {
-    this.props.mutator['tenants'].POST(data).then(() =>
+    this.props.mutator.tenants.POST(data).then(() =>
       this.context.router.transitionTo('/okapi-console/tenants')
       );
   }
 
-  cancel(data) {
+  cancel() {
     this.context.router.transitionTo('/okapi-console/tenants');
   }
 
   render() {
-    return (
-        <TenantForm onSubmit={this.create.bind(this)} 
-                    cancelForm={this.cancel.bind(this)} 
-                    submitLabel='Add' />
-    );
+    return <TenantForm
+      onSubmit={this.create}
+      cancelForm={this.cancel}
+      submitLabel="Add"
+    />;
   }
 }
 
