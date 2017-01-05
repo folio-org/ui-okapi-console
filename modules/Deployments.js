c995e7295782e2dddce224c4c428ae4323009ae5
Mike Taylor <mike@indexdata.com>
2017-01-05 10:01:42 +0000

    Many ESLint errors fixed.

--- modules/Deployments.js
+++ modules/Deployments.js
@@ -1,79 +1,89 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'stripes-connect'; // eslint-disable-line
-import DeploymentForm from './DeploymentForm'
-
+import DeploymentForm from './DeploymentForm';
 
 export class Deployments extends Component {
+  static propTypes = {
+    data: PropTypes.object,
+    mutator: PropTypes.shape({
+      deployment_modules: PropTypes.shape({
+        DELETE: PropTypes.func.isRequired,
+        POST: PropTypes.func.isRequired,
+      })
+    }),
+    refreshRemote: PropTypes.func,
+    srvcId: PropTypes.string,
+  };
 
-  static manifest = { 'discovery_modules':  { type: 'okapi',
-                                              path: '_/discovery/modules',
-                                              pk: 'instId'
-                                            },
-                      'discovery_nodes':    { type: 'okapi',
-                                              path: '_/discovery/nodes',
-                                              pk:   'nodeId'
-                                            },
-                      'deployment_modules': { type: 'okapi',
-                                              path: '_/deployment/modules', 
-                                              pk: 'instId',
-                                              clientGeneratePk: false
-                                            }
-                    };
+  static manifest = {
+    discovery_modules: {
+      type: 'okapi',
+      path: '_/discovery/modules',
+      pk: 'instId'
+    },
+    discovery_nodes: {
+      type: 'okapi',
+      path: '_/discovery/nodes',
+      pk: 'nodeId'
+    },
+    deployment_modules: {
+      type: 'okapi',
+      path: '_/deployment/modules',
+      pk: 'instId',
+      clientGeneratePk: false
+    }
+  };
 
   constructor(props) {
     super(props);
+    this.deleteDeploy = this.deleteDeploy.bind(this);
+    this.addDeploy = this.addDeploy.bind(this);
   }
 
-  addDeploy (data) {
-    this.props.mutator['deployment_modules'].POST(data).then(() => {
-      this.props.refreshRemote(this.props)
-    }
-    );
-  }
-
-  deleteDeploy (data) {
-    this.props.mutator['deployment_modules'].DELETE(data).then(() =>
+  addDeploy(data) {
+    this.props.mutator.deployment_modules.POST(data).then(() =>
       this.props.refreshRemote(this.props)
     );
   }
 
-  render () {
+  deleteDeploy(data) {
+    this.props.mutator.deployment_modules.DELETE(data).then(() =>
+      this.props.refreshRemote(this.props)
+    );
+  }
 
-    const { data: { discovery_modules, discovery_nodes }, 
-            srvcId 
+  render() {
+    const { data: { discovery_modules: dm, discovery_nodes: dn },
+            srvcId
           } = this.props;
 
-    if (! discovery_nodes || discovery_nodes.length == 0 ) return <div/>;
-    let nextindex = (discovery_modules ? discovery_modules.length : 0);
+    if (!dn || dn.length === 0) return <div />;
+    const nextindex = (dm ? dm.length : 0);
     return (
-     <div>
-     {discovery_modules.map
-       ((deployment, index) => {
-           if (deployment.srvcId===srvcId) {
-             return (
-               <DeploymentForm
-                form={'dep-' + index.toString()}
-                key={'dep-' + index.toString()}
-                deployNodes={discovery_nodes}
-                initialValues={deployment}
-                onSubmit={this.deleteDeploy.bind(this)}
-                disable={true}
-               />
-             );
-           }
-         }
-       )
-     }
-     <br/>
-     <DeploymentForm 
-       form={'dep-' + nextindex.toString()}
-       key={'dep-' + nextindex.toString()}
-       deployNodes={discovery_nodes}
-       initialValues={ {srvcId: srvcId} }
-       onSubmit={this.addDeploy.bind(this)}
-       disable={false}
-     />
-     </div>
+      <div>
+        {dm.map((deployment, index) => {
+          if (deployment.srvcId !== srvcId)
+            return <span />;
+
+          return <DeploymentForm
+            form={'dep-' + index.toString()}
+            key={'dep-' + index.toString()}
+            deployNodes={dn}
+            initialValues={deployment}
+            onSubmit={this.deleteDeploy}
+            disable
+          />;
+        })}
+        <br />
+        <DeploymentForm
+          form={'dep-' + nextindex.toString()}
+          key={'dep-' + nextindex.toString()}
+          deployNodes={dn}
+          initialValues={{ srvcId }}
+          onSubmit={this.addDeploy}
+          disable={false}
+        />
+      </div>
     );
   }
 }
