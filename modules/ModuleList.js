191fed523d35a88878efc37d8530f308f3e10889
Mike Taylor <mike@indexdata.com>
2017-01-05 12:34:50 +0000

    ESLint fixes for ModuleList.js

--- modules/ModuleList.js
+++ modules/ModuleList.js
@@ -1,36 +1,46 @@
 import React, { Component } from 'react';
-import { connect } from 'stripes-connect';
+import { connect } from 'stripes-connect'; // eslint-disable-line
 import { Link } from 'react-router';
 
 class ModuleList extends Component {
+  static propTypes = {
+    mutator: React.PropTypes.shape({
+      modules: React.PropTypes.shape({
+        DELETE: React.PropTypes.func.isRequired,
+      }),
+    }),
+    data: React.PropTypes.shape({}),
+    pathname: React.PropTypes.string,
+  };
+
   static manifest = {
-    'modules': { type: 'okapi',
-                 path: '_/proxy/modules'
-               }
+    modules: {
+      type: 'okapi',
+      path: '_/proxy/modules'
+    }
   };
 
   render() {
-    const { data: {modules}, mutator, pathname } = this.props;
+    const { data: { modules }, mutator, pathname } = this.props;
 
     if (!modules) return null;
-    var moduleNodes = modules.map((amodule) => {
-      return (
-        <li key={amodule.id}>
-          {amodule.name}&nbsp;
-          [<Link to={`${pathname}/edit/${amodule.id}`}>Edit</Link>]
-          [<a onClick={() => mutator['modules'].DELETE(amodule)}>delete</a>]
-        </li>
-      );
-    });
+    const moduleNodes = modules.map(amodule =>
+      <li key={amodule.id}>
+        {amodule.name}&nbsp;
+        <button><Link to={`${pathname}/edit/${amodule.id}`}>Edit</Link></button>
+        <button onClick={() => mutator.modules.DELETE(amodule)}>Delete</button>
+      </li>
+    );
+
     return (
       <div>
         <div>
-        <h3>Module list:</h3>
-        <ul>
-          {moduleNodes}
-        </ul>
+          <h3>Module list:</h3>
+          <ul>
+            {moduleNodes}
+          </ul>
         </div>
-         <Link to={`${pathname}/add`}>Add module</Link>
+        <Link to={`${pathname}/add`}>Add module</Link>
       </div>
     );
   }
