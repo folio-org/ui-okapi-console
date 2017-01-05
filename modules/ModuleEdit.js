53f3ec75c670f42f96513060460c45d2b0f46b1b
Mike Taylor <mike@indexdata.com>
2017-01-05 12:28:33 +0000

    ESLint fixes for ModuleEdit.js

--- modules/ModuleEdit.js
+++ modules/ModuleEdit.js
@@ -1,41 +1,59 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'stripes-connect';
+import { connect } from 'stripes-connect'; // eslint-disable-line
 import ModuleForm from './ModuleForm';
 import removeEmpty from '../utils/removeEmptyObjectsFromArrays';
 
 
 class ModuleEdit extends Component {
+  static propTypes = {
+    mutator: PropTypes.shape({
+      modules: PropTypes.shape({
+        PUT: PropTypes.func.isRequired,
+      })
+    }),
+    data: PropTypes.object,
+    params: PropTypes.object
+  };
+
   static contextTypes = {
     router: PropTypes.object.isRequired
   };
 
-  static manifest = { 'modules': { type: 'okapi',
-                                   path: '_/proxy/modules/:{moduleid}'
-                                 }
-                    };
+  static manifest = {
+    modules: {
+      type: 'okapi',
+      path: '_/proxy/modules/:{moduleid}'
+    }
+  };
+
+  constructor(props) {
+    super(props);
+    this.update = this.update.bind(this);
+    this.cancel = this.cancel.bind(this);
+  }
 
   update(data) {
     removeEmpty(data);
-    this.props.mutator.modules.PUT(data).then(()=>
+    this.props.mutator.modules.PUT(data).then(() =>
       this.context.router.transitionTo('/okapi-console/modules')
-    )
+    );
   }
 
-  cancel(data) {
+  cancel() {
     this.context.router.transitionTo('/okapi-console/modules');
   }
 
   render() {
-    const { data: {modules}, mutator, params: {moduleid} } = this.props;
+    const { data: { modules }, params: { moduleid } } = this.props;
 
     if (!modules) {
-      return <div/>;
+      return <div />;
     }
-    let module = modules.find((module) =>  { return module.id === moduleid });
+    const module = modules.find(m => m.id === moduleid);
     if (!module) {
-      return <div/>;
+      return <div />;
     }
-    return <ModuleForm initialValues={module} onSubmit={this.update.bind(this)} cancelForm={this.cancel.bind(this)} />
+    return <ModuleForm initialValues={module} onSubmit={this.update} cancelForm={this.cancel} />;
   }
 }
 
