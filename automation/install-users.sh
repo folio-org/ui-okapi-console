6346593bbb5920011f8b199980a4a4d40b0a1ad0
Mike Taylor <mike@indexdata.com>
2017-01-26 21:41:16 +0000

    Make assumptions more explicit. Add section to load sample users. Fixes part of STRIPES-166.

--- automation/install-users.sh
+++ automation/install-users.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# Assumes that Okapi is already running
+# Assumptions:
+# * mod-users is checked out next to ui-okapi-console
+# * folio-ansible is checked out next to ui-okapi-console
+# * Okapi is already running
+
 OKAPI_URL=http://localhost:9130
 TMP=/tmp/okapi.91222
 #trap 'rm -f $TMP' 0 1 15
@@ -30,8 +34,17 @@ srvcId=`sed -n 's/.*srvcId" : "\(.*\)",/\1/p' $TMP`
 echo srvcId=$srvcId
 
 if true; then
-# Enable "diku" tenant to use "users" module
+# Enable the "users" module for the "diku" tenant
 curl -X POST -w '\n' -D - -H 'Content-type: application/json' \
     -d '{"id": "users-module"}' \
     $OKAPI_URL/_/proxy/tenants/diku/modules
 fi
+
+# Add sample users
+for f in ../../folio-ansible/roles/mod-users-data/files/*; do
+    curl -w '\n' -X POST -D - \
+        -H "Content-type: application/json" \
+        -H "X-Okapi-Tenant: diku" \
+        -d @$f http://localhost:9130/users
+    done
+
