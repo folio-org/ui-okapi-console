c6dee0ce1fa8387695c2c97fd372d3f9be986f66
Mike Taylor <mike@indexdata.com>
2017-01-26 21:44:16 +0000

    New file 01-install-tenant.sh, extracted from start of install-users.sh

---
+++ automation/01-install-tenant.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Assumptions:
+# * Okapi is already running
+
+OKAPI_URL=http://localhost:9130
+
+# Create the "diku" tenant
+curl -X POST -w '\n' -D - -H 'Content-type: application/json' \
+    -d '{"id": "diku", "name": "Diku"}' \
+    $OKAPI_URL/_/proxy/tenants
+fi
