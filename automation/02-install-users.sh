0911f9f785431c43a4ed1e38a2225d44596db949
Mike Taylor <mike@indexdata.com>
2017-01-27 14:22:21 +0000

    Remove unnecessary "if true" guards.

--- automation/02-install-users.sh
+++ automation/02-install-users.sh
@@ -9,29 +9,23 @@ OKAPI_URL=http://localhost:9130
 TMP=/tmp/okapi.91222
 #trap 'rm -f $TMP' 0 1 15
 
-if true; then
 # Set up proxying for "users" module
 curl -X POST -w '\n' -D - -H 'Content-type: application/json' \
     -d @../../mod-users/ModuleDescriptor.json \
     $OKAPI_URL/_/proxy/modules
-fi
 
-if true; then
 # Deploy the "users" module
 curl -X POST -w '\n' -D - -H 'Content-type: application/json' \
     -d @../../mod-users/DeploymentDescriptor.json \
     $OKAPI_URL/_/deployment/modules | tee $TMP
-fi
 
 srvcId=`sed -n 's/.*srvcId" : "\(.*\)",/\1/p' $TMP`
 echo srvcId=$srvcId
 
-if true; then
 # Enable the "users" module for the "diku" tenant
 curl -X POST -w '\n' -D - -H 'Content-type: application/json' \
     -d '{"id": "users-module"}' \
     $OKAPI_URL/_/proxy/tenants/diku/modules
-fi
 
 # Add sample users
 for f in ../../folio-ansible/roles/mod-users-data/files/*; do
