328a9d82b4711a23821eafa5a06727e732b8f5da
Mike Taylor <mike@indexdata.com>
2017-02-01 15:12:40 +0000

    New script to generate sample items

---
+++ automation/sample-items/csv2json_items.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+use IO::File;
+
+my %ids;
+while (my $line = (<>)) {
+    next if $line =~ /^#/;
+    chomp($line);
+    $line =~ s/"/\\"/g;
+    my($id, $author, $name, $year, $notes) = split(',', $line);
+    next if $id eq 'id';
+
+    die "$ARGV:$.:duplicate id '$id'" if $ids{$id};
+    $ids{$id} = 1;
+
+    my $fileName = "sample-item$id.json";
+    my $f = new IO::File(">$fileName")
+	or die "can't create file '$fileName': $!";
+
+    print $f <<__EOT__;
+{
+  "id": "$id",
+  "instanceId": "x-$id",
+  "title": "$name",
+  "author": "$author",
+  "date": "$year",
+  "comment": "$notes"
+}
+
+__EOT__
+
+    $f->close();
+    last;
+}
