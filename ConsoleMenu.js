9641e355d3475bb5d8d65883a2e326a354f1b208
Michal Kuklis <michal.kuklis@gmail.com>
2017-06-02 11:54:33 -0400

    Upgrade react and stripes dependencies

--- ConsoleMenu.js
+++ ConsoleMenu.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Link from 'react-router-dom/Link';
 
-export default function () {
-  return <div>
-    Okapi Console Menu
-    [<Link to={'/okapi-console/tenants'}>Tenants</Link>]
-    [<Link to={'/okapi-console/modules'}>Modules</Link>]
-    [<Link to={'/okapi-console/health/health'}>Health</Link>]
-    [<Link to={'/okapi-console/health/healthconnected'}>Health Connected</Link>]
-  </div>;
-}
+export default () => <div>
+  Okapi Console Menu
+  [<Link to={'/okapi-console/tenants'}>Tenants</Link>]
+  [<Link to={'/okapi-console/modules'}>Modules</Link>]
+  [<Link to={'/okapi-console/health/health'}>Health</Link>]
+  [<Link to={'/okapi-console/health/healthconnected'}>Health Connected</Link>]
+</div>;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import Match from 'react-router/Match';
+import React, { PropTypes, Component } from 'react';
+import Route from 'react-router-dom/Route';
+import Switch from 'react-router-dom/Switch';
+
 import ConsoleMenu from './ConsoleMenu';
 import TenantList from './tenants/TenantList';
 import TenantAdd from './tenants/TenantAdd';
@@ -10,22 +12,25 @@ import ModuleEdit from './modules/ModuleEdit';
 import Health from './health/Health';
 import HealthConnected from './health/HealthConnected';
 
-
-const Users = ({ pathname }) => <div>
+const Users = ({ match: { path } }) => <div>
   <ConsoleMenu />
   <hr />
-  <Match exactly pattern={`${pathname}/tenants`} component={TenantList} />
-  <Match pattern={`${pathname}/tenants/edit/:tenantid`} component={TenantEdit} />
-  <Match pattern={`${pathname}/tenants/add`} component={TenantAdd} />
-  <Match exactly pattern={`${pathname}/modules`} component={ModuleList} />
-  <Match pattern={`${pathname}/modules/edit/:moduleid`} component={ModuleEdit} />
-  <Match pattern={`${pathname}/modules/add`} component={ModuleAdd} />
-  <Match exactly pattern={`${pathname}/health/health`} component={Health} />
-  <Match exactly pattern={`${pathname}/health/healthconnected`} component={HealthConnected} />
+  <Switch>
+    <Route exact path={`${path}/tenants`} component={TenantList} />
+    <Route path={`${path}/tenants/edit/:tenantid`} component={TenantEdit} />
+    <Route path={`${path}/tenants/add`} component={TenantAdd} />
+    <Route exact path={`${path}/modules`} component={ModuleList} />
+    <Route path={`${path}/modules/edit/:moduleid`} component={ModuleEdit} />
+    <Route path={`${path}/modules/add`} component={ModuleAdd} />
+    <Route exact path={`${path}/health/health`} component={Health} />
+    <Route exact path={`${path}/health/healthconnected`} component={HealthConnected} />
+  </Switch>
 </div>;
 
-Users.propTypes = {
-  pathname: React.PropTypes.string.isRequired,
+Users.propTypes= {
+  match: PropTypes.shape({
+    path: PropTypes.string.isRequired
+  }).isRequired
 };
 
 export default Users;
